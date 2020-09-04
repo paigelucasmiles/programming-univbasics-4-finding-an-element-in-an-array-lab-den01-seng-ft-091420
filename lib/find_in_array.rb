25335c9b93d600a2f4687eeffd338032ddd64721
Paige Miles <paige@lucas-miles.com>
2020-09-04 03:11:03 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,5 +5,7 @@ def find_element_index(array, value_to_find)
   while count < array.length do
     puts array[count] == value_to_find
       count += 1
+      binding.pry
+      puts "goodbye"
   end
 end
