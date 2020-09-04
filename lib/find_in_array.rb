f7749a553a5d6ae241c468fba7ae622f728928aa
Paige Miles <paige@lucas-miles.com>
2020-09-04 03:16:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,12 +2,10 @@ require 'pry'
 
 def find_element_index(array, value_to_find)
   count = 0
-  search_value_index = nil
   while count < array.length do
     if array[count] == value_to_find
-      search_value_index = count
+      return count
+      end
+      count += 1
+      end
     end
-    count += 1
-  end
-  search_value_index
-end
