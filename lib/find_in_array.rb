6f1d3f4c82da68ce33faf5622eea3e0eb7553b3f
Paige Miles <paige@lucas-miles.com>
2020-09-04 03:09:45 +0000

    Automatically backed up by Learn

---
+++ lib/find_in_array.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+def find_element_index(array, value_to_find)
+  count = 0
+  while count < array.length do
+    if array[count] == value_to_find
+      puts count
+    else 
+      return nil
+      count += 1
+    end
+  end
+end
