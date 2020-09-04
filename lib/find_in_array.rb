9fe9218ad7324954695e94771265fc1b559c0b85
Paige Miles <paige@lucas-miles.com>
2020-09-04 03:15:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def find_element_index(array, value_to_find)
   count = 0
+  search_value_index = nil
   while count < array.length do
-    puts array[count] == value_to_find
-      count += 1
-      binding.pry
-      puts "goodbye"
+    if array[count] == value_to_find
+      search_value_index = count
+    end
+    count += 1
   end
+  search_value_index
 end
